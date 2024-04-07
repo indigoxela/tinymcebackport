fd2ee00c3f9044de44ae81d9c52023e8fee29fde
indigoxela <xela@indigofloat.at>
2024-04-07 16:14:07 +0200

    Rudimentary Media module support for file picker (#2)

---
+++ js/tinymce-drupalmedia.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * Override method from Media module.
+ */
+(function (Drupal, $) {
+  "use strict";
+
+  Drupal.media.browser.submit = function (event) {
+    // If we have a field context, this isn't triggered by the editor. Do what
+    // the original function does then and jump out.
+    let $fieldContext = $(parent.window.document.body).find('#mediaBrowser');
+    if ($fieldContext.length) {
+      let $buttons = $fieldContext.parent('.ui-dialog').find('.ui-dialog-buttonpane button');
+      $buttons[0].click();
+      return false;
+    }
+
+    event.preventDefault();
+    let selection = Drupal.media.browser.selectedMedia;
+    if (selection.length) {
+      let file = selection[0];
+      let result = {
+        url: file.url,
+        name: file.filename,
+        alt: file.alt,
+        type: file.type,
+      };
+      if (file.width && file.height) {
+        result.width = file.width + '';
+        result.height = file.height + '';
+      }
+      // Communicate with TinyMCE drupalmedia plugin.
+      window.parent.postMessage(result, window.location.origin);
+    }
+  };
+})(Drupal, jQuery);
---
+++ plugins/drupalmedia/plugin.js
@@ -0,0 +1,53 @@
+/**
+ * @file
+ * TinyMCE Drupal media browser plugin.
+ */
+(function () {
+
+  'use strict';
+
+  tinymce.PluginManager.add('drupalmedia', function(editor, url) {
+    editor.on('PreInit', function () {
+      editor.options.register('mediaBrowseUrl', { processor: 'string' });
+
+      const mediaBrowser = function (callback, value, meta) {
+        // Controller to remove event listener. Prevent multiple events attached.
+        const controller = new AbortController();
+
+        // Let TinyMCE communicate with Drupal media.
+        window.addEventListener('message', function (event) {
+          if (event.origin !== window.location.origin) {
+            return;
+          }
+          let mediaType = event.data.type;
+          if (meta.filetype === 'image' && mediaType === 'image') {
+            callback(event.data.url, {
+              width: event.data.width,
+              height: event.data.height
+            });
+          }
+          else if (meta.filetype === 'file') {
+            callback(event.data.url, { text: event.data.name });
+          }
+          else if (meta.filetype === 'media' && (mediaType === 'video' || mediaType === 'audio')) {
+            callback(event.data.url, {});
+          }
+          editor.windowManager.close();
+          // Job done, remove listener.
+          controller.abort();
+        }, { signal: controller.signal }, false);
+
+        // Open a TinyMCE dialog with IMCE.
+        editor.windowManager.openUrl({
+          title: 'Media browser',
+          url: editor.options.get('mediaBrowseUrl'),
+          onCancel: function (api) {
+            controller.abort();
+          }
+        });
+      };
+      editor.options.set('file_picker_callback', mediaBrowser);
+    });
+  });
+
+})();
